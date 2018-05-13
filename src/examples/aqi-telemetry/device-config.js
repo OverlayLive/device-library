6aeeed2b408ce37bd7ebd82e71cae19df4b55996
Tim <xyala@overclocking-tv.com>
2018-05-13 20:35:50 +0800

    documentation

--- src/examples/aqi-telemetry/device-config.js
+++ src/examples/aqi-telemetry/device-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiKey: '3813ef95a069ab1cfd0be781216ff62869311c34',
+  apiKey: '3813ef95a069ab1cfd0be781216ff62869311c34',       // Get you API key from your user dashboard : htts://my.overlay.live
   ingest: 'ingest.epeakgears.com',
-  deviceKey: 'd_4111'
+  deviceKey: 'd_4111'                                       // To get a device key, add a manual device to your account.
 }
