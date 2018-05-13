d0f4a12359290dff5ad223c202a19abcc9c6b3f2
Tim <xyala@overclocking-tv.com>
2018-05-13 17:34:49 +0800

    added WAQI config file / dynamic WAQI URL build in the WAQI reader module

---
+++ src/examples/aqi-telemetry/waqi-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url: 'http://api.waqi.info/feed/',                        // Get you API data for a single location. More info : http://aqicn.org/json-api/doc/
+  token: '4bc4b13a1a250e7e4f264972602e574790aae32a',        // Get your own token for free at http://aqicn.org/api/
+  uid: '1599'                                             //UID of the city you want to query
+}
