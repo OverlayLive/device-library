b97e93a39b8ece36a6f4a272bc64851b7bd218c5
Tim <xyala@overclocking-tv.com>
2018-05-18 17:00:09 +0800

    fix to take care of eventual offline sensors

--- src/examples/aqi-telemetry/waqi-reader.js
+++ src/examples/aqi-telemetry/waqi-reader.js
@@ -33,32 +33,68 @@ function WAQIReader(manager, refreshInterval, config_waqi) {
 
             console.log('--------------');
 
-            console.log('time: '+body.data.time.s);
-            manager.publish('aqi-s1-time', body.data.time.s);
+            if(body.data.hasOwnProperty('time')){
+              console.log('time: '+body.data.time.s);
+              manager.publish('aqi-s1-time', body.data.time.s);
+            }else {
+                console.log('temperature offline');
+            }
 
-            console.log('PM 2.5: '+body.data.iaqi.pm25.v);
-            manager.publish('aqi-s2-pm25', body.data.iaqi.pm25.v);
+            if(body.data.iaqi.hasOwnProperty('pm25')){
+              console.log('PM 2.5: '+body.data.iaqi.pm25.v);
+              manager.publish('aqi-s2-pm25', body.data.iaqi.pm25.v);
+            }else {
+                console.log('pm25 offline');
+            }
 
-            console.log('PM 10: '+body.data.iaqi.pm10.v);
-            manager.publish('aqi-s3-pm10', body.data.iaqi.pm10.v);
+            if(body.data.iaqi.hasOwnProperty('pm10')){
+              console.log('PM 10: '+body.data.iaqi.pm10.v);
+              manager.publish('aqi-s3-pm10', body.data.iaqi.pm10.v);
+            }else {
+                console.log('pm10 offline');
+            }
 
-            console.log('CO2: '+ body.data.iaqi.co.v);
-            manager.publish('aqi-s4-co2', body.data.iaqi.co.v);
+            if(body.data.iaqi.hasOwnProperty('co')){
+              console.log('CO2: '+ body.data.iaqi.co.v);
+              manager.publish('aqi-s4-co2', body.data.iaqi.co.v);
+            }else {
+                console.log('co offline');
+            }
 
-            console.log('NO2: '+ body.data.iaqi.no2.v);
-            manager.publish('aqi-s5-no2', body.data.iaqi.no2.v);
+            if(body.data.iaqi.hasOwnProperty('no2')){
+              console.log('NO2: '+ body.data.iaqi.no2.v);
+              manager.publish('aqi-s5-no2', body.data.iaqi.no2.v);
+            }else {
+                console.log('no2 offline');
+            }
 
-            console.log('SO2: '+ body.data.iaqi.so2.v);
-            manager.publish('aqi-s6-so2', body.data.iaqi.so2.v);
+            if(body.data.iaqi.hasOwnProperty('so2')){
+              console.log('SO2: '+ body.data.iaqi.so2.v);
+              manager.publish('aqi-s6-so2', body.data.iaqi.so2.v);
+            }else {
+                console.log('so2 offline');
+            }
 
-            console.log('O3: '+ body.data.iaqi.o3.v);
-            manager.publish('aqi-s7-o3', body.data.iaqi.o3.v);
+            if(body.data.iaqi.hasOwnProperty('o3')){
+              console.log('O3: '+ body.data.iaqi.o3.v);
+              manager.publish('aqi-s7-o3', body.data.iaqi.o3.v);
+            }else {
+                console.log('o3 offline');
+            }
 
-            console.log('temperature: '+body.data.iaqi.t.v);
-            manager.publish('aqi-s8-temperature', body.data.iaqi.t.v);
+            if(body.data.iaqi.hasOwnProperty('t')){
+              console.log('temperature: '+body.data.iaqi.t.v);
+              manager.publish('aqi-s8-temperature', body.data.iaqi.t.v);
+            }else {
+                console.log('temperature offline');
+            }
 
-            console.log('ATM pressure: '+body.data.iaqi.p.v);
-            manager.publish('aqi-s9-atmpressure', body.data.iaqi.p.v);
+            if(body.data.iaqi.hasOwnProperty('p')){
+              console.log('ATM pressure: '+body.data.iaqi.p.v);
+              manager.publish('aqi-s9-atmpressure', body.data.iaqi.p.v);
+            }else {
+                console.log('atm pressure offline');
+            }
         }
     })
   }
